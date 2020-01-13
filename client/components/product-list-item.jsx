51c5d21c5f217c3c098977c7bb3384b57e8e48fe
Christopher Ward <chrsward90@gmail.com>
2020-01-13 15:57:54 -0800

    Initial definition of ProductListItem

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ProductListItem(props) {
+  return (
+    <div className="card-body">
+      <div className="card">
+        <h4 className="card-title">Card Title</h4>
+      </div>
+    </div>
+  );
+}
