ad22af7b9154756d708b24ba3ffdce7661d598fa
Christopher Ward <chrsward90@gmail.com>
2020-01-13 15:43:50 -0800

    Defined header component for store that is used in App's render method

---
+++ client/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header(props) {
+  return (
+    <div className="container-fluid header">
+      <div className="row align-items-center justify-content-start ml-5">
+        <i className="fas fa-dollar-sign"></i>
+        <h3 className="ml-1">Wicked Sales</h3>
+      </div>
+    </div>
+  );
+}
