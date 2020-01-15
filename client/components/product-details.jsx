e35188d9278b1aca28603d7736911b0d6dff1973
Christopher Ward <chrsward90@gmail.com>
2020-01-14 16:02:45 -0800

    componentDidMount method added in product-details to get from server all product properties by productId

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -8,6 +8,16 @@ export default class ProductDetails extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch(`/api/products/${this.state.product.productId}`)
+      .then(response => {
+        return response.json();
+      })
+      .catch(err => {
+        console.error('Caught in ProductDetails.componentDidMount:', err);
+      });
+  }
+
   render() {
     return null;
   }
