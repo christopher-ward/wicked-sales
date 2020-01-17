ab5dbf1131343761b109828b33f88d50ed81607d
Christopher Ward <chrsward90@gmail.com>
2020-01-17 10:01:44 -0800

    defined addToCart method to PUSH new cart item to server before updating cart state in App

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,8 +8,10 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       view: {
-        name: 'catalog',
-        params: {},
+        name: 'details', // hardcoded to details to work on that section, default is 'catalog'
+        params: {
+          productId: 3 // hardcoded to work on add to cart button, default is params: {},
+        },
         cart: []
       }
     };
@@ -27,6 +29,33 @@ export default class App extends React.Component {
       })
       .catch(err => {
         console.error('Caught in App.getCartItems:', err);
+      });
+  }
+
+  addToCart(product) {
+    const fetchURL = '/api/cart';
+    const initObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    };
+    const fetchRequest = new Request(fetchURL, initObj);
+    fetch(fetchRequest)
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        const newCart = this.state.view.cart.concat(response);
+        this.setState({
+          view: {
+            cart: newCart
+          }
+        });
+      })
+      .catch(err => {
+        console.error('Caught in App.addToCart:', err);
       });
   }
 
@@ -50,7 +79,7 @@ export default class App extends React.Component {
     }
     return (
       <>
-        <Header />
+        <Header cartItemCount={this.state.view.cart.length}/>
         <ProductDetails
           productId={this.state.view.params.productId}
           view={this.setView}/>
