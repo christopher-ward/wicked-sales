cc19ceb237f386aff15be199d5e25a9ea56d6b0f
Christopher Ward <chrsward90@gmail.com>
2020-03-20 23:27:31 -0700

    Inputs are evaluated on blur

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -18,47 +18,197 @@ export default class CheckoutForm extends React.Component {
       creditCardVisualFeedback: '',
       cardExpDateVisualFeedback: '',
       cardSecCodeVisualFeedback: '',
-      addressVisualFeedback: ''
+      addressVisualFeedback: '',
+      inputSelected: '', // inputSelected will hold the name of the input targetted on click of input
+      prevInput: '' // prevInput will be used to check previous input field
+      // This will be used to check input when switching to different input.
     };
     this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.filterCharacters = this.filterCharacters.bind(this);
   }
 
   handleFormChange(event) {
-    const value = event.target.type === 'number' && event.target.length > 0 ? parseInt(event.target.value) : event.target.value;
-    const name = event.target.name;
-    // this.inputCheck();
-    if (name === 'name' && value.length > 65) {
-      return;
-    } else if (name === 'creditCard' && value.length > 16) {
-      return;
-    } else if (name === 'cardExpDate' && value.length > 7) {
-      return;
-    } else if (name === 'cardCVV' && value.length > 4) {
-      return;
-    } else if (name === 'shippingAddress' && value.length > 156) {
-      return;
-    }
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    if (value.match(/\D/g) && target.classList.contains('number')) { return; }
+    // this.inputCheck(target);
     this.setState({
       [name]: value
     });
   }
 
+  handleFocus(event) {
+    this.setState({
+      prevInput: event.target
+    });
+  }
+
+  handleBlur(event) {
+    const { prevInput } = this.state;
+    switch (prevInput.name) {
+      case 'name':
+        if (prevInput.value.length < 5) {
+          this.setState({
+            nameCheck: 'Full name required!',
+            nameVisualFeedback: 'fa-times'
+          });
+        } else {
+          this.setState({
+            nameCheck: '',
+            nameVisualFeedback: 'fa-check'
+          });
+        }
+        break;
+      case 'creditCard':
+        if (prevInput.value.length < 16) {
+          this.setState({
+            creditCardCheck: 'Credit Card number required!',
+            creditCardVisualFeedback: 'fa-times'
+          });
+        } else {
+          this.setState({
+            creditCardCheck: '',
+            creditCardVisualFeedback: 'fa-check'
+          });
+        }
+        break;
+      case 'cardExpDate':
+        if (prevInput.value.length < 7) {
+          this.setState({
+            cardExpDateCheck: 'Exp. Date required!',
+            cardExpDateVisualFeedback: 'fa-times'
+          });
+        } else {
+          this.setState({
+            cardExpDateCheck: '',
+            cardExpDateVisualFeedback: 'fa-check'
+          });
+        }
+        break;
+      case 'cardCVV':
+        if (prevInput.value.length < 3) {
+          this.setState({
+            cardCVVCheck: 'CVV required!',
+            cardSecCodeVisualFeedback: 'fa-times'
+          });
+        } else {
+          this.setState({
+            cardCVVCheck: '',
+            cardSecCodeVisualFeedback: 'fa-check'
+          });
+        }
+        break;
+      case 'shippingAddress':
+        if (prevInput.value.length < 21) {
+          this.setState({
+            addressCheck: 'Shipping Address required!',
+            addressVisualFeedback: 'fa-times'
+          });
+        } else {
+          this.setState({
+            addressCheck: '',
+            addressVisualFeedback: 'fa-check'
+          });
+        }
+        break;
+      default:
+        // eslint-disable-next-line no-console
+        console.log('Something went wrong in the switch');
+    }
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    const orderObj = {
-      name: this.state.name,
-      creditCard: this.state.creditCard,
-      shippingAddress: this.state.shippingAddress
-    };
-    if (!this.inputCheck()) {
+    if (!this.submitCheck()) {
       return;
     }
+    const orderObj = {
+      name: this.state.name,
+      creditCard: parseInt(this.state.creditCard),
+      shippingAddress: this.state.shippingAddress
+    };
     this.props.placeOrder(orderObj);
   }
 
-  inputCheck() {
+  inputCheck(prevInput) {
+    if (prevInput) {
+      switch (prevInput.name) {
+        case 'name':
+          if (prevInput.value.length < 5) {
+            this.setState({
+              nameCheck: 'Full name required!',
+              nameVisualFeedback: 'fa-times'
+            });
+          } else {
+            this.setState({
+              nameCheck: '',
+              nameVisualFeedback: 'fa-check'
+            });
+          }
+          break;
+        case 'creditCard':
+          if (prevInput.value.length < 16) {
+            this.setState({
+              creditCardCheck: 'Credit Card number required!',
+              creditCardVisualFeedback: 'fa-times'
+            });
+          } else {
+            this.setState({
+              creditCardCheck: '',
+              creditCardVisualFeedback: 'fa-check'
+            });
+          }
+          break;
+        case 'cardExpDate':
+          if (prevInput.value.length < 7) {
+            this.setState({
+              cardExpDateCheck: 'Exp. Date required!',
+              cardExpDateVisualFeedback: 'fa-times'
+            });
+          } else {
+            this.setState({
+              cardExpDateCheck: '',
+              cardExpDateVisualFeedback: 'fa-check'
+            });
+          }
+          break;
+        case 'cardCVV':
+          if (prevInput.value.length < 3) {
+            this.setState({
+              cardCVVCheck: 'CVV required!',
+              cardSecCodeVisualFeedback: 'fa-times'
+            });
+          } else {
+            this.setState({
+              cardCVVCheck: '',
+              cardSecCodeVisualFeedback: 'fa-check'
+            });
+          }
+          break;
+        case 'shippingAddress':
+          if (prevInput.value.length < 21) {
+            this.setState({
+              addressCheck: 'Shipping Address required!',
+              addressVisualFeedback: 'fa-times'
+            });
+          } else {
+            this.setState({
+              addressCheck: '',
+              addressVisualFeedback: 'fa-check'
+            });
+          }
+          break;
+        default:
+          // eslint-disable-next-line no-console
+          console.log('something went wrong in switch');
+      }
+    }
+  }
+
+  submitCheck() {
     const { name, creditCard, cardExpDate, cardCVV, shippingAddress } = this.state;
     if (name.length < 5 || creditCard.length < 16 || cardExpDate.length < 7 || cardCVV.length < 3 || shippingAddress.length < 21) {
       if (name.length < 5) {
@@ -121,10 +271,6 @@ export default class CheckoutForm extends React.Component {
     return true;
   }
 
-  filterCharacters(event) {
-    ['e', 'E', '+', '-', '.'].includes(event.key) && event.preventDefault();
-  }
-
   render() {
     const nameResultVisual = this.state.nameVisualFeedback;
     const creditCardResultVisual = this.state.creditCardVisualFeedback;
@@ -142,7 +288,10 @@ export default class CheckoutForm extends React.Component {
                 name="name"
                 id="name"
                 value={this.state.name}
-                onChange={this.handleFormChange} />
+                onChange={this.handleFormChange}
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
+                maxLength={65} />
               <div className="d-flex">
                 <i className={`fas ${nameResultVisual}`} />
                 <small>{this.state.nameCheck}</small>
@@ -152,12 +301,15 @@ export default class CheckoutForm extends React.Component {
               <div className="d-flex col-lg-6 col-12 flex-column form-group">
                 <label htmlFor="creditCard">Credit Card</label>
                 <input
-                  type="number"
+                  type="text"
                   name="creditCard"
                   id="creditCard"
+                  className="number"
                   value={this.state.creditCard}
                   onChange={this.handleFormChange}
-                  onKeyDown={this.filterCharacters} />
+                  onFocus={this.handleFocus}
+                  onBlur={this.handleBlur}
+                  maxLength={16} />
                 <div className="d-flex">
                   <i className={`fas ${creditCardResultVisual}`} />
                   <small>{this.state.creditCardCheck}</small>
@@ -166,12 +318,15 @@ export default class CheckoutForm extends React.Component {
               <div className="d-flex flex-column col-lg-4 col-8 form-group">
                 <label htmlFor="cardExpDate">Expiration Date</label>
                 <input
-                  type="number"
+                  type="text"
                   name="cardExpDate"
                   id="cardExpDate"
+                  className="number"
                   value={this.state.cardExpDate}
                   onChange={this.handleFormChange}
-                  onKeyDown={this.filterCharacters} />
+                  onFocus={this.handleFocus}
+                  onBlur={this.handleBlur}
+                  maxLength={7} />
                 <div className="d-flex">
                   <i className={`fas ${cardExpDateResultVisual}`} />
                   <small>{this.state.cardExpDateCheck}</small>
@@ -180,12 +335,15 @@ export default class CheckoutForm extends React.Component {
               <div className="d-flex flex-column col-lg-2 col-4 form-group">
                 <label htmlFor="cardCVV">CVV</label>
                 <input
-                  type="number"
+                  type="text"
                   name="cardCVV"
                   id="cardCVV"
+                  className="number"
                   value={this.state.cardCVV}
                   onChange={this.handleFormChange}
-                  onKeyDown={this.filterCharacters} />
+                  onFocus={this.handleFocus}
+                  onBlur={this.handleBlur}
+                  maxLength={4} />
                 <div className="d-flex">
                   <i className={`fas ${cardCVVResultVisual}`} />
                   <small>{this.state.cardCVVCheck}</small>
@@ -199,7 +357,10 @@ export default class CheckoutForm extends React.Component {
                 name="shippingAddress"
                 id="address"
                 value={this.state.shippingAddress}
-                onChange={this.handleFormChange} />
+                onChange={this.handleFormChange}
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
+                maxLength={156} />
               <div className="d-flex">
                 <i className={`fas ${addressResultVisual}`} />
                 <small>{this.state.addressCheck}</small>
@@ -209,7 +370,6 @@ export default class CheckoutForm extends React.Component {
           </form>
         </div>
       </div>
-
     );
   }
 }
